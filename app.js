c535d8ae7a5ae11ffd1e28f78cc46ae712ddad8c
hrachgalstyan <hrachgalstyann@gmail.com>
2020-07-06 21:02:35 +0400

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,6 +26,16 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// Implement cors
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, natours.com
+// app.use(cors({
+//     origin: 'https://natours.com'
+// }))
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
 
 // Serving static files
 // app.use(express.static(`${__dirname}/public`));
